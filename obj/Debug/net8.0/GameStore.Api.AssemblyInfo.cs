562b7368ba6e29ae37cef9c3066ef149fe40a657
Ikenna Benedict Nwaka <inwaka39@gmail.com>
2025-10-03 08:43:06 +0200

    CreateGameDto

--- obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
+++ obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55ec494c967034483866d5a04e6afc5983022c4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b345e6c8573d1c8006936b90b5e9670b65d84bc7")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
